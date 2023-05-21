6550a43392ebb0fb7d3cf2f13817ac340edd8d1b
KIHM-02 <monteroisaac33@gmail.com>
2023-05-20 22:19:59 -0600

    Added add-to-start, delete, and display data

--- src/listacircular/Lista.java
+++ src/listacircular/Lista.java
@@ -1,11 +1,75 @@
 package listacircular;
 
 /**
- *
- * @author USER
+ * @author Kevin Huerta Montero 22310411
  */
 
 public class Lista 
 {
+    public Nodo inicio;
+    
+    public Lista()
+    {
+        inicio = null;
+    }
+    
+    public boolean isEmpty()
+    {
+        if(inicio == null)
+            return true;
+        
+        return false;
+    }
+    
+    public void begin(int dato)
+    {
+        Nodo nuevo;
+        
+        if(isEmpty())
+        {
+            nuevo = new Nodo(dato, null);
+            inicio = nuevo;
+            System.out.println("Se inserto el dato "+dato+" en Inicio");
+        }
+        else
+        {
+            nuevo = new Nodo(dato, inicio);
+            inicio = nuevo;
+            System.out.println("Se agrego el dato "+dato);
+        }
+    }
+    
+    public void showBegin()
+    {
+        Nodo temporal;
+        
+        if(isEmpty())
+        {
+            System.out.println("La lista está vacía");
+        }
+        else
+        {
+            temporal = inicio;
+            while(temporal != null)
+            {
+                System.out.println(" "+temporal.getDato());
+                temporal = temporal.getSiguiente();
+            }
+        }
+    }
+    
+    public void deleteBegin()
+    {
+        Nodo temporal;
+        
+        if(isEmpty())
+        {
+            System.out.println("La lista está vacía");
+        }
+        else
+        {
+            inicio = inicio.getSiguiente();
+        }
+    }
     
 }
--- src/listacircular/Main.java
+++ src/listacircular/Main.java
@@ -7,6 +7,19 @@ public class Main
 {
     public static void main(String[] args) 
     {
+        Lista lista = new Lista();
         
+        lista.begin(1);
+        lista.begin(2);
+        lista.begin(3);
+        lista.begin(4);
+        
+        lista.showBegin();
+        lista.deleteBegin();
+        
+        System.out.println("----");
+        
+        lista.showBegin();
     }
 }
+
