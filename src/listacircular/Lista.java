4c51d964a62513caad7082105d2bede1fb537a1f
KIHM-02 <monteroisaac33@gmail.com>
2023-05-21 20:19:51 -0600

    Added add-to-last and delete in circle list

--- src/listacircular/Lista.java
+++ src/listacircular/Lista.java
@@ -31,15 +31,20 @@ public class Lista
         return false;
     }
     
+    public void firstAdd(int dato)
+    {
+        Nodo nuevo = new Nodo(dato, null);
+        inicio = nuevo;
+        System.out.println("Se agrego el dato "+dato+" en Inicio");
+    }
+    
     public void begin(int dato)
     {
         Nodo nuevo;
         
         if(inicioIsEmpty())
         {
-            nuevo = new Nodo(dato, null);
-            inicio = nuevo;
-            System.out.println("Se agrego el dato "+dato+" en Inicio");
+            firstAdd(dato);
         }
         else
         {
@@ -65,7 +70,7 @@ public class Lista
     {
         if(inicioIsEmpty())
         {
-            System.out.println("La lista está vacía");
+            System.out.println("La lista esta vacia");
         }
         else
         {
@@ -87,7 +92,7 @@ public class Lista
         
         if(inicioIsEmpty())
         {
-            System.out.println("La lista está vacía");
+            System.out.println("La lista esta vacia");
         }
         else
         {
@@ -103,6 +108,67 @@ public class Lista
                     temporal = temporal.getSiguiente();
                 }
                 System.out.println(" "+ultimo.getDato());
+            }
+        }
+    }
+    
+    // Seccion agregar al final
+    
+    public void last(int dato)
+    {
+        Nodo nuevo;
+        
+        if(inicioIsEmpty())
+        {
+            firstAdd(dato);
+        }
+        else
+        {
+            if(ultimoIsEmpty())
+            {
+                nuevo = new Nodo(dato, inicio);
+                inicio.setSiguiente(nuevo);
+                ultimo = nuevo;
+                System.out.println("Se agrego el dato "+dato+" al final");
+            }
+            else
+            {
+                nuevo = new Nodo(dato, inicio);
+                ultimo.setSiguiente(nuevo);
+                ultimo = nuevo;
+                System.out.println("Se agrego el dato "+dato+" al nuevo final");
+            }
+        }
+    }
+    
+    public void deleteLast()
+    {
+        Nodo temporal, nuevo;
+        
+        if(inicioIsEmpty())
+        {
+            System.out.println("La lista esta vacia");
+        }
+        else
+        {
+            if(ultimoIsEmpty())
+            {
+                inicio = null;
+            }
+            else
+            {
+                temporal = inicio;
+                nuevo = inicio;
+                
+                while(temporal != ultimo)
+                {
+                    nuevo = temporal;
+                    temporal = temporal.getSiguiente();
+                }
+                
+                ultimo.setSiguiente(null);
+                nuevo.setSiguiente(inicio);
+                ultimo = nuevo;
             }
         }
     }
--- src/listacircular/Main.java
+++ src/listacircular/Main.java
@@ -9,6 +9,7 @@ public class Main
     {
         Lista lista = new Lista();
         
+        /*
         lista.begin(1);
         lista.begin(2);
         lista.begin(3);
@@ -18,9 +19,37 @@ public class Main
         
         System.out.println("--");
         
-        lista.deleteBegin();
+        //lista.deleteBegin();
         
         lista.showData();
+        
+        System.out.println("--");
+        */
+        
+        lista.last(1);
+        lista.last(2);
+        lista.last(3);
+        lista.last(4);
+        
+        lista.showData();
+        
+        System.out.println("--");
+        
+        lista.begin(0);
+        lista.last(5);
+        
+        System.out.println("--");
+        
+        lista.showData();
+        
+        System.out.println("---");
+        
+        lista.deleteLast();
+        lista.last(6);                                                          //comprobamos que la lista sigue funcionando
+        lista.begin(-1); 
+        
+        lista.showData();
+        
         
     }
 }
